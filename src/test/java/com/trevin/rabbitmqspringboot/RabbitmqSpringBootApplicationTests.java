4af7b8474d729ac931b48802d400c61a42098283
trevin-livele <liveletrevin6@gmail.com>
2022-12-20 12:36:25 -0500

    {configuration} :wrench: set up rabbitmq configs

---
+++ src/test/java/com/trevin/rabbitmqspringboot/RabbitmqSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trevin.rabbitmqspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
